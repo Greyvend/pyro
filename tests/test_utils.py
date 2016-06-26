8e54a607346af0e2df817e85d11078dd310939cf
Serge Mosin <svmosin@gmail.com>
2016-06-26 21:20:19 +0600

    utils tests were updated to new attributes representation.

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -6,9 +6,12 @@ from pyro.utils import containing_relation, min_dict, all_equal
 class TestContainingRelation(TestCase):
     def test_no_containing_relation(self):
         attribute = 'G'
-        r1 = {'name': 'R1', 'attributes': {'A', 'B'}, 'pk': {'A'}}
-        r2 = {'name': 'R2', 'attributes': {'C', 'D'}, 'pk': {'C'}}
-        r3 = {'name': 'R3', 'attributes': {'E', 'F'}, 'pk': {'E'}}
+        r1 = {'name': 'R1', 'attributes': {'A': 'INT', 'B': 'INT'},
+              'pk': {'A'}}
+        r2 = {'name': 'R2', 'attributes': {'C': 'INT', 'D': 'INT'},
+              'pk': {'C'}}
+        r3 = {'name': 'R3', 'attributes': {'E': 'INT', 'F': 'INT'},
+              'pk': {'E'}}
         relations = [r1, r2, r3]
 
         self.assertRaises(ValueError, containing_relation, relations,
@@ -16,18 +19,24 @@ class TestContainingRelation(TestCase):
 
     def test_one_containing_relation(self):
         attribute = 'D'
-        r1 = {'name': 'R1', 'attributes': {'A', 'B'}, 'pk': {'A'}}
-        r2 = {'name': 'R2', 'attributes': {'C', 'D'}, 'pk': {'C'}}
-        r3 = {'name': 'R3', 'attributes': {'E', 'F'}, 'pk': {'E'}}
+        r1 = {'name': 'R1', 'attributes': {'A': 'INT', 'B': 'INT'},
+              'pk': {'A'}}
+        r2 = {'name': 'R2', 'attributes': {'C': 'INT', 'D': 'INT'},
+              'pk': {'C'}}
+        r3 = {'name': 'R3', 'attributes': {'E': 'INT', 'F': 'INT'},
+              'pk': {'E'}}
         relations = [r1, r2, r3]
 
         self.assertDictEqual(containing_relation(relations, attribute), r2)
 
     def test_two_containing_relations(self):
         attribute = 'B'
-        r1 = {'name': 'R1', 'attributes': {'A', 'B'}, 'pk': {'A'}}
-        r2 = {'name': 'R2', 'attributes': {'B', 'D'}, 'pk': {'B'}}
-        r3 = {'name': 'R3', 'attributes': {'E', 'F'}, 'pk': {'E'}}
+        r1 = {'name': 'R1', 'attributes': {'A': 'INT', 'B': 'INT'},
+              'pk': {'A'}}
+        r2 = {'name': 'R2', 'attributes': {'B': 'INT', 'D': 'INT'},
+              'pk': {'B'}}
+        r3 = {'name': 'R3', 'attributes': {'E': 'INT', 'F': 'INT'},
+              'pk': {'E'}}
 
         self.assertDictEqual(containing_relation([r1, r2, r3], attribute), r1)
         self.assertDictEqual(containing_relation([r2, r1, r3], attribute), r2)

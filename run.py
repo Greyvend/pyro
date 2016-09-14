27d795b2dcec91ff31f08bbd491b4ceb1ae419c6
Serge Mosin <svmosin@gmail.com>
2016-09-14 12:06:51 +0600

    Minor refactoring: redundant file was removed. Logging was added to run module.

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 import json
+import logging
 
 from sqlalchemy import create_engine
 from sqlalchemy.engine.url import URL
@@ -7,11 +8,16 @@ from pyro import db, tj, transformation
 from pyro.utils import relation_name
 
 
+logging.basicConfig(level=logging.INFO)
+
+
 if __name__ == '__main__':
     with open('config.json') as config_file:
         config = json.load(config_file)
 
     # connect to source Database
+    logging.info('Connecting to the source DB: {}'.format(
+        config['source_db']['database']))
     source_engine = create_engine(URL(**config['source_db']))
 
     relations, dependencies = db.get_schema(source_engine)
@@ -28,10 +34,13 @@ if __name__ == '__main__':
 
     # Start transformation
     # build contexts
+    logging.info('Building dimension contexts...')
     contexts = []
     base_total = {relation_name(measure)}
     # build dimension contexts
     for dimension in config['dimensions']:
+        logging.info('Building context for dimension "{}"'.format(
+            dimension['name']))
         # extract relation names from extended attribute names in config
         base = set(map(relation_name, dimension['attributes']))
         base_total |= base
@@ -40,16 +49,26 @@ if __name__ == '__main__':
         contexts.append(context)
 
     # build application context
+    logging.info('Building application context')
     try:
         app_context = next(transformation.contexts(relations, base_total,
                                                    dependencies))
     except StopIteration:
         # no combination satisfies Lossless Join property. Pick all relations
+        logging.warning('No lossless combinations were found, picking whole '
+                        'relation set for application application context')
         app_context = relations
     contexts.append(app_context)
 
     # connect to the output DB
+    logging.info('Connecting to the cube DB: {}'.format(
+        config['cube_db']['database']))
     cube_engine = create_engine(URL(**config['cube_db']))
 
     for context in contexts:
+        logging.info('Building Table of Joins for the context {}'.format(
+            context))
         tj.build(context, dependencies, source_engine, cube_engine)
+
+    logging.info('The source Database has been successfully transformed to '
+                 'OLAP representation!')

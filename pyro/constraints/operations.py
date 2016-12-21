29b49ff699e48ad299eb13deda5b9cbe911491da
Serge Mosin <svmosin@gmail.com>
2016-12-21 18:55:11 +0600

    operations module was extended with the following functions: `_is_predicate_domain_included`, `_is_conjunction_clause_domain_included`, `is_domain_included`. Tests were created as well.

--- pyro/constraints/operations.py
+++ pyro/constraints/operations.py
@@ -1,3 +1,6 @@
+from pyro.constraints.domains import factory
+
+
 def project(constraint, attributes):
     """
     Perform F\X, a contraction projection. It means that from F\X(t) = TRUE it
@@ -12,3 +15,61 @@ def project(constraint, attributes):
                    in attributes]
                   for conjunction_clause in constraint]
     return list(filter(None, projection))
+
+
+def _is_predicate_domain_included(p1, p2):
+    """
+    Prime function performing predicate domain comparison.
+
+    :param p1: first predicate
+    :type p1: dict
+    :param p2: second predicate
+    :type p2: dict
+    :return: True if domain of p2 is a superset of p1's domain
+    """
+    if p1['attribute'] != p2['attribute']:
+        return False
+    dom_1 = factory(p1)
+    dom_2 = factory(p2)
+    return dom_1.issubset(dom_2)
+
+
+def _is_conjunction_clause_domain_included(clause_1, clause_2):
+    """
+    Find out whether M(clause_1) is a subset of M(clause_2),
+    where M - constraint domain. It holds iff clause_1 -> clause_2 ~ TRUE.
+
+    :param clause_1: first conjunction clause
+    :type clause_1:  lists of dicts
+    :param clause_2: second conjunction clause
+    :type clause_2:  list of lists of dicts
+    :return: True if M(clause_1) is subset of M(clause_2), False otherwise
+    """
+    for predicate_2 in clause_2:
+        for predicate_1 in clause_1:
+            if _is_predicate_domain_included(predicate_1, predicate_2):
+                break
+        else:
+            return False
+    return True
+
+
+def is_domain_included(c1, c2):
+    """
+    Find out whether M(c1) is a subset of M(c2), where M - constraint domain.
+    It holds iff c1 -> c2 ~ TRUE.
+
+    :param c1: first logical constraint
+    :type c1:  list of lists of dicts
+    :param c2: second logical constraint
+    :type c2:  list of lists of dicts
+    :return: True if M(c1) is subset of M(c2), False otherwise
+    """
+    for conjunction_clause_1 in c1:
+        for conjunction_clause_2 in c2:
+            if _is_conjunction_clause_domain_included(conjunction_clause_1,
+                                                      conjunction_clause_2):
+                break
+        else:
+            return False
+    return True
--- tests/test_constraints/test_operations.py
+++ tests/test_constraints/test_operations.py
@@ -1,6 +1,7 @@
 from unittest import TestCase
 
-from pyro.constraints.operations import project
+from pyro.constraints.operations import project, is_domain_included, \
+    _is_predicate_domain_included
 
 
 class TestProject(TestCase):
@@ -31,3 +32,108 @@ class TestProject(TestCase):
 
         self.assertEqual(len(projection), 0)
         self.assertEqual(projection, [])
+
+
+class TestIsPredicateDomainIncluded(TestCase):
+    """
+    Some corner cases for single predicates
+    """
+    def test_between_predicates(self):
+        p1 = {'attribute': 'A1', 'operation': 'BETWEEN', 'value': [3, 15]}
+        p2 = {'attribute': 'A1', 'operation': 'BETWEEN', 'value': [2, 11]}
+
+        self.assertFalse(_is_predicate_domain_included(p1, p2))
+        self.assertFalse(_is_predicate_domain_included(p2, p1))
+
+
+class TestIsDomainIncluded(TestCase):
+    def test_single_conjunction_clause_successful(self):
+        c1 = [
+            [{'attribute': 'A1', 'operation': '=', 'value': 3},
+             {'attribute': 'A2', 'operation': '<', 'value': 179},
+             {'attribute': 'A3', 'operation': '<=', 'value': 13}]
+        ]
+        c2 = [
+            [{'attribute': 'A1', 'operation': 'IN', 'value': [3, 5, 6]},
+             {'attribute': 'A2', 'operation': '<=', 'value': 179}]
+        ]
+
+        self.assertTrue(is_domain_included(c1, c2))
+        self.assertFalse(is_domain_included(c2, c1))
+
+    def test_single_conjunction_clause_different_attributes(self):
+        c1 = [
+            [{'attribute': 'A1', 'operation': '=', 'value': 3},
+             {'attribute': 'A2', 'operation': '<', 'value': 179}]
+        ]
+        c2 = [
+            [{'attribute': 'A2', 'operation': '<>', 'value': 16},
+             {'attribute': 'A3', 'operation': '<=', 'value': 13}]
+        ]
+
+        self.assertFalse(is_domain_included(c1, c2))
+        self.assertFalse(is_domain_included(c2, c1))
+
+    def test_single_conjunction_clause_direct_subset(self):
+        c1 = [
+            [{'attribute': 'A1', 'operation': '=', 'value': 3},
+             {'attribute': 'A2', 'operation': '<', 'value': 179}]
+        ]
+        c2 = [
+            [{'attribute': 'A1', 'operation': '=', 'value': 3},
+             {'attribute': 'A2', 'operation': '<', 'value': 179},
+             {'attribute': 'A3', 'operation': '<=', 'value': 13}]
+        ]
+
+        self.assertFalse(is_domain_included(c1, c2))
+        self.assertTrue(is_domain_included(c2, c1))
+
+    def test_multiple_conjunction_clauses_partially_included(self):
+        c1 = [
+            [{'attribute': 'A1', 'operation': '=', 'value': 3}],
+            [{'attribute': 'A2', 'operation': 'BETWEEN', 'value': [3, 15]}],
+        ]
+        c2 = [
+            [{'attribute': 'A3', 'operation': 'LIKE', 'value': '%sfdabc%'}],
+            [{'attribute': 'A2', 'operation': 'BETWEEN', 'value': [6, 11]}],
+            [{'attribute': 'A1', 'operation': '>', 'value': 0}]
+        ]
+
+        self.assertFalse(is_domain_included(c1, c2))
+        self.assertFalse(is_domain_included(c2, c1))
+
+    def test_multiple_conjunction_clauses_included(self):
+        c1 = [
+            [{'attribute': 'A1', 'operation': '=', 'value': 3},
+             {'attribute': 'A2', 'operation': '<', 'value': 179}],
+            [{'attribute': 'A4', 'operation': 'BETWEEN', 'value': [3, 15]},
+             {'attribute': 'A3', 'operation': 'LIKE', 'value': 'abc%'}],
+        ]
+        c2 = [
+            [{'attribute': 'A3', 'operation': 'LIKE', 'value': '%sfdabc%'}],
+            [{'attribute': 'A4', 'operation': 'BETWEEN', 'value': [2, 17]},
+             {'attribute': 'A3', 'operation': 'LIKE', 'value': 'abc%'}],
+            [{'attribute': 'A1', 'operation': '>', 'value': 0}]
+        ]
+
+        self.assertTrue(is_domain_included(c1, c2))
+        self.assertFalse(is_domain_included(c2, c1))
+
+    def test_multiple_conjunction_clauses_not_included(self):
+        """
+        First conjunction clause's domain is a subset of first one in second
+        constraint but second clause doesn't have any corresponding clause.
+        """
+        c1 = [
+            [{'attribute': 'A1', 'operation': '=', 'value': 3},
+             {'attribute': 'A2', 'operation': '<', 'value': 179}],
+            [{'attribute': 'A1', 'operation': 'BETWEEN', 'value': [3, 15]},
+             {'attribute': 'A3', 'operation': 'LIKE', 'value': 'abc%'}],
+        ]
+        c2 = [
+            [{'attribute': 'A1', 'operation': '=', 'value': 3},
+             {'attribute': 'A2', 'operation': '<', 'value': 179}]
+        ]
+
+        self.assertFalse(is_domain_included(c1, c2))
+        self.assertTrue(is_domain_included(c2, c1))

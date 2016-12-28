f44e1cb185d5eb99252e33418cf7794aedac7df0
Serge Mosin <svmosin@gmail.com>
2016-12-28 20:05:42 +0600

    `Cache` class was updated: config now automatically creates if not exists. Besides, `add` method was fixed: SQLAlchemy types are properly serialized, Unicode symbols are correctly written.

--- pyro/cache.py
+++ pyro/cache.py
@@ -1,15 +1,29 @@
 import json
+from json import JSONDecodeError
 
 from pyro import db
 from pyro.constraints.operations import is_domain_included
+from pyro.utils import SQLAlchemySerializer
 
 
 class Cache:
     def __init__(self, engine, file):
         self._engine = engine
         self._file = file
-        with open(file) as config_file:
-            self._config = json.load(config_file)
+        self._config = None
+        self._relation = None
+        self.enabled = False
+
+        self.read_config()
+
+    def read_config(self):
+        with open(self._file, 'a+') as config_file:
+            config_file.seek(0)
+            try:
+                self._config = json.load(config_file)
+            except JSONDecodeError:
+                self._config = []
+                json.dump(self._config, config_file)
         self._relation = None
         self.enabled = False
 
@@ -34,7 +48,8 @@ class Cache:
                      'constraint': constraint}
         self._config.append(new_entry)
         with open(self._file, 'w') as config_file:
-            json.dump(self._config, config_file)
+            json.dump(self._config, config_file, cls=SQLAlchemySerializer,
+                      ensure_ascii=False)
 
     def restore(self, dest_relation, *constraints):
         db.insert_from_select(self._engine, dest_relation,
